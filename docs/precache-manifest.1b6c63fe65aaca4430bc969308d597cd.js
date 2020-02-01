f1e1197d755dc70a69007e7fb678d70dcece9afb
Sumedh Chakravorty <sumedh@Sumedhs-MacBook-Pro.local>
2020-02-02 01:17:15 +0530

    added demo on github pages

---
+++ docs/precache-manifest.1b6c63fe65aaca4430bc969308d597cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdafc69bb34e8c8c08b0cd1334e58fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6926dd2451159856fc26",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64069b832f975bb302ef",
+    "url": "/static/js/2.109547b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.109547b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6926dd2451159856fc26",
+    "url": "/static/js/main.3c5ca488.chunk.js"
+  },
+  {
+    "revision": "09346c936a333c580a33",
+    "url": "/static/js/runtime-main.c693427f.js"
+  }
+]);
